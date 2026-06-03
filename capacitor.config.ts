1509dbdc111d9ee44b7fc96a40519d7bf3ec3627
Sathwik Kusuri <sathwik.kusuri@infosharesystems.com>
2026-06-03 20:29:20 +0530

    Add Capacitor configuration and Android project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.timelessclicks.app',
+  appName: 'TimelessClicks',
+  webDir: 'dist'
+};
+
+export default config;
